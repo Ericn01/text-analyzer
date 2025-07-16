50c94c38b696d0b8171c4f1f981cc29a0b84dd1c
Ericn01 <nielseneric2001@gmail.com>
2025-07-16 13:55:22 -0600

    Added basic drag and drop file functionality.

---
+++ src/app/components/AnalyzingState.tsx
@@ -0,0 +1,11 @@
+const AnalyzingState = () => {
+    return (
+        <section id="analyzingState">
+            <div className="animate-spin"></div>
+            <h2> Analyzing your document... </h2>
+            <p>This may take a few moments depending on the document size</p>
+        </section>
+    )
+}
+
+export default AnalyzingState;
